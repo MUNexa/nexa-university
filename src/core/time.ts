6419b7edb30139dc023ed30366d059f1e90c5b8d
MUNexa <hrudayasahu191@gmail.com>
2026-01-05 23:43:06 +0530

    Update time.ts

--- src/core/time.ts
+++ src/core/time.ts
@@ -1,1 +1,16 @@
+export class time {
+  static delta = 0;
+  static elapsed = 0;
+  private static last = 0;
 
+  static init() {
+    this.last = performance.now();
+  }
+
+  static update() {
+    const now = performance.now();
+    this.delta = (now - this.last) / 1000;
+    this.elapsed += this.delta;
+    this.last = now;
+  }
+}
