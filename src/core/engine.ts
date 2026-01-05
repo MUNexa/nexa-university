4c9362060d15ba6e666d3fb708c6de0e77774d41
MUNexa <hrudayasahu191@gmail.com>
2026-01-05 23:47:51 +0530

    Update engine.ts

--- src/core/engine.ts
+++ src/core/engine.ts
@@ -1,1 +1,26 @@
 
+import { event_bus } from "./event_bus";
+import { time } from "./time";
+
+export class engine {
+  private running = false;
+
+  start() {
+    if (this.running) return;
+    this.running = true;
+    time.init();
+    this.loop();
+  }
+
+  private loop = () => {
+    if (!this.running) return;
+
+    time.update();
+    event_bus.emit("engine_tick", {
+      delta: time.delta,
+      elapsed: time.elapsed
+    });
+
+    requestAnimationFrame(this.loop);
+  };
+}
