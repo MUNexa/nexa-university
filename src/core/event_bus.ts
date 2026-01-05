56d5788c4e90d703703080168fb9458aaa57f6fb
MUNexa <hrudayasahu191@gmail.com>
2026-01-05 23:42:48 +0530

    Update event_bus.ts

--- src/core/event_bus.ts
+++ src/core/event_bus.ts
@@ -1,1 +1,16 @@
+type callback = (payload?: any) => void;
 
+export class event_bus {
+  private static events = new Map<string, callback[]>();
+
+  static on(event: string, cb: callback) {
+    if (!this.events.has(event)) this.events.set(event, []);
+    this.events.get(event)!.push(cb);
+  }
+
+  static emit(event: string, payload?: any) {
+    const listeners = this.events.get(event);
+    if (!listeners) return;
+    listeners.forEach(cb => cb(payload));
+  }
+}
