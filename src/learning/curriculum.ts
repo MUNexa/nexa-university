084d0ba8a49a61b8a1edc5f9565f02461f8d8def
MUNexa <hrudayasahu191@gmail.com>
2026-01-06 00:04:43 +0530

    Update curriculum.ts

--- src/learning/curriculum.ts
+++ src/learning/curriculum.ts
@@ -1,1 +1,20 @@
+import { lesson } from "./lesson";
 
+export class curriculum {
+  private lessons: lesson[] = [];
+  private index = 0;
+
+  add_lesson(lesson: lesson) {
+    this.lessons.push(lesson);
+  }
+
+  current(): lesson | null {
+    return this.lessons[this.index] || null;
+  }
+
+  advance() {
+    if (this.index < this.lessons.length - 1) {
+      this.index++;
+    }
+  }
+}
