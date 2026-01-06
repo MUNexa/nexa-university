a2e8d4be233cb5345320f1ddafa6bb95db9009e2
MUNexa <hrudayasahu191@gmail.com>
2026-01-06 21:24:42 +0530

    Update wallix_behavior.ts

--- src/mentors/wallix/wallix_behavior.ts
+++ src/mentors/wallix/wallix_behavior.ts
@@ -1,1 +1,11 @@
 
+export class wallix_behavior {
+  sad_count = 0;
+
+  react_to_failure(failures: number) {
+    if (failures >= 5) {
+      this.sad_count++;
+      console.log("WALLIX looks sad...");
+    }
+  }
+}
