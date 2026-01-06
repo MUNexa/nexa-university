7514a615346793aa0622158d8238cc0da65e08e0
MUNexa <hrudayasahu191@gmail.com>
2026-01-06 21:28:13 +0530

    Update baylex.ts

--- src/mentors/baylex/baylex.ts
+++ src/mentors/baylex/baylex.ts
@@ -1,1 +1,19 @@
+import { mentor_base } from "../mentor_base";
+import { baylex_dialogue } from "./baylex_dialogue";
 
+export class baylex extends mentor_base {
+  dialogue: baylex_dialogue;
+
+  constructor() {
+    super("baylex", "neutral, logical, no emotions");
+    this.dialogue = new baylex_dialogue();
+  }
+
+  greet() {
+    this.dialogue.say("Hello. I am your mentor companion. Rate your learning level 1-10.");
+  }
+
+  teach(topic: string) {
+    this.dialogue.say(`We will study ${topic} with precision and logic.`);
+  }
+}
