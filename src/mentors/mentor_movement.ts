2d1625310e4831c96944902229af1d7b72b01c78
MUNexa <hrudayasahu191@gmail.com>
2026-01-06 21:20:48 +0530

    Update mentor_movement.ts

--- src/mentors/mentor_movement.ts
+++ src/mentors/mentor_movement.ts
@@ -1,1 +1,12 @@
 
+export class mentor_movement {
+  position = { x: 0, y: 0, z: 0 };
+
+  move_to(x: number, y: number, z: number) {
+    this.position = { x, y, z };
+  }
+
+  teleport(x: number, y: number, z: number) {
+    this.position = { x, y, z };
+  }
+}
