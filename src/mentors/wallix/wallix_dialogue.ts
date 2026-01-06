071ca96db68a7be2734eaaa287ae032381a47b04
MUNexa <hrudayasahu191@gmail.com>
2026-01-06 21:26:33 +0530

    Update wallix_dialogue.ts

--- src/mentors/wallix/wallix_dialogue.ts
+++ src/mentors/wallix/wallix_dialogue.ts
@@ -1,25 +1,5 @@
-import { mentor_base } from "../mentor_base";
-import { wallix_behavior } from "./wallix_behavior";
-import { wallix_visuals } from "./wallix_visuals";
-import { wallix_dialogue } from "./wallix_dialogue";
-
-export class wallix extends mentor_base {
-  behavior: wallix_behavior;
-  visuals: wallix_visuals;
-  dialogue: wallix_dialogue;
-
-  constructor() {
-    super("wallix", "funny, emotional, happy by default");
-    this.behavior = new wallix_behavior();
-    this.visuals = new wallix_visuals();
-    this.dialogue = new wallix_dialogue();
-  }
-
-  greet() {
-    this.dialogue.say("Hello! I am WALLIX, your coding mentor.");
-  }
-
-  teach(topic: string) {
-    this.dialogue.say(`Let's explore ${topic} together!`);
+export class wallix_dialogue {
+  speak(message: string) {
+    console.log("WALLIX:", message);
   }
 }
