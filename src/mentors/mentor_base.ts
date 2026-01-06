868257edd56f20ddfc09aa7e047a92627d6cd7a0
MUNexa <hrudayasahu191@gmail.com>
2026-01-06 21:19:56 +0530

    Update mentor_base.ts

--- src/mentors/mentor_base.ts
+++ src/mentors/mentor_base.ts
@@ -1,1 +1,25 @@
+import { event_bus } from "../core/event_bus";
 
+export abstract class mentor_base {
+  name: string;
+  personality: string;
+  visible: boolean = true;
+
+  constructor(name: string, personality: string) {
+    this.name = name;
+    this.personality = personality;
+  }
+
+  abstract greet(): void;
+  abstract teach(topic: string): void;
+
+  hide() {
+    this.visible = false;
+    event_bus.emit("mentor_visibility_change", { mentor: this.name, visible: false });
+  }
+
+  show() {
+    this.visible = true;
+    event_bus.emit("mentor_visibility_change", { mentor: this.name, visible: true });
+  }
+}
