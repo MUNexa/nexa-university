11e8e2fdb7d53889e01f4db25baf30d1136bbc7a
MUNexa <hrudayasahu191@gmail.com>
2026-01-06 21:28:50 +0530

    Update baylex_dialogue.ts

--- src/mentors/baylex/baylex_dialogue.ts
+++ src/mentors/baylex/baylex_dialogue.ts
@@ -1,1 +1,9 @@
+export class baylex_dialogue {
+  say(text: string) {
+    console.log("BAYLEX:", text);
+  }
 
+  show_solution(solution: string) {
+    console.log("BAYLEX shows solution:", solution);
+  }
+}
